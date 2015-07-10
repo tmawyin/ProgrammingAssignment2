c7efad6163f3eca474d7712f9279693d54159df0
Tomas Mawyin <tmawyin@gmail.com>
2015-07-10 17:31:29 -0400

    Started the code for the makeCacheMatrix function that defines a new cached matrix object

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Coursera
+## by Tomas Mawyin
+## Assignment 2
 
-## Write a short comment describing this function
+## The following script is designed to cache the inverse of a matrix.
+## The function is used to store the inverse of a matrix in case the matrix
+## does not change instead of calculating the inverse every time.
+## This is advantageous for really large matrices as in the test case shown
+
+## makeCacheMatrix: creates an "chache matrix" object that performs four
+## basic operations: sets or gets the data as specified by the user, and
+## sets or gets the cached inverse matrix.
+## Each line of code is commented to make it clear.
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+    # Define the initial variable holding the inverse of the matrix
+    inverse <- NULL
+    
+    # SET: sets the data in x based on the user defined yMatrix variable
+    setInverse <- function(yMatrix) {
+        # yMatrix is stored as x from the parent environment 
+        x <<- yMatrix
+        inverse <<- NULL
+    } # End of setInverse
+    
+    # 
+    
+} # End of makeCacheMatrix
 
 
 ## Write a short comment describing this function
